e5cf89a86938cb76719a90da5a07e8ce6e442fcf
Rishabhjain-92 <rishabhjain92148@gmail.com>
2025-12-26 21:09:21 +0530

    auth middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 
@@ -19,6 +19,10 @@ router.route("/register").post(
     ]),
     registerUser
 )
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT,logoutUser)
 
 
 export default router
--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -17,7 +17,8 @@ cloudinary.config({
                 resource_type: "auto"
             })
             // file has been uploaded successfully
-            console.log("file is uploaded on cloudinary",response.url);
+            // console.log("file is uploaded on cloudinary",response.url);
+            fs.unlinkSync(localFilePath)// remove the locally saved temporary file
             return response;
 
             
